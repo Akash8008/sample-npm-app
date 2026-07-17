39a02c87ab69f8ab2f464d26fc8a811b9bf52c25
Akash8008 <akashpendyala123@gmail.com>
2026-07-17 21:23:40 +0530

    updates code

--- build.js
+++ build.js
@@ -9,4 +9,13 @@ fs.rmSync(outDir, { recursive: true, force: true });
 fs.mkdirSync(outDir, { recursive: true });
 fs.copyFileSync(sourceFile, outFile);
 
-console.log('Build completed: dist/index.js generated.');
+// generate a server.js entry that starts the app (some Dockerfiles expect dist/server.js)
+const serverFile = path.join(outDir, 'server.js');
+const serverContents = "const app = require('./index');\n" +
+	"const PORT = process.env.PORT || 3000;\n" +
+	"app.listen(PORT, () => {\n" +
+	"  console.log('Server is running on http://localhost:' + PORT);\n" +
+	"});\n";
+fs.writeFileSync(serverFile, serverContents, { encoding: 'utf8' });
+
+console.log('Build completed: dist/index.js and dist/server.js generated.');
